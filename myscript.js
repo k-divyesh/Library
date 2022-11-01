6f928b72fad69f1dc15d9066f5de80bb6bf98c53
divyesh <kdivyesh.cs@gmail.com>
2022-11-01 20:33:11 +0400

    base js library array book object

---
+++ myscript.js
@@ -0,0 +1,20 @@
+library = []
+
+function book (name, author, pages, read){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read= read;
+
+    library.push(this)
+}
+
+const book1 = new book("atomic habbits","james charles", 32, true)
+const book2 = new book("richdadpoordad", "robertKiyosaki", 300, true)
+book.prototype.info = function() {
+    console.log(`${this.bookName}${this.author}${this.pages}${this.read}`)
+}
+
+
+
+console.log(library)
